cbc418f6338a08b42822948c66aa7c15d72b626e
Sam Aaron <samaaron@gmail.com>
2010-09-13 10:37:25 +0100

    initial commit

---
+++ src/blinken_park/core.clj
@@ -0,0 +1,1 @@
+(ns blinken_park.core)
---
+++ test/blinken_park/test/core.clj
@@ -0,0 +1,6 @@
+(ns blinken_park.test.core
+  (:use [blinken_park.core] :reload)
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
